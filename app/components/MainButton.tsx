8476027d36f6576a1ad88950d3f68e467ebee48e
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-04-11 13:32:43 +0530

    dhfk

--- app/components/MainButton.tsx
+++ app/components/MainButton.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, ReactElement } from "react";
-// import { Loader2 } from "lucide-react";
+import { Loader2 } from "lucide-react";
 import { Button } from "./button";
 
 type MainButtonProps = {
